25df484f3176a52f364b20c7cf3e132b23bc23e7
JoannaAch <JoannaAch@users.noreply.github.com>
2019-09-11 08:57:42 +1000

    Update run_hic-pro_lncap.sh

--- HiC-Pro/run_hic-pro_lncap.sh
+++ HiC-Pro/run_hic-pro_lncap.sh
@@ -1,1 +1,1 @@
-time /home/joaach/HiC-Pro_2/bin/HiC-Pro -c config_hic-pro_lncap -i fastq_HiC-Pro_LNCaP  -o hicpro_lncap
+time /home/joaach/HiC-Pro_2/bin/HiC-Pro -c config_hic-pro_lncap -i fastq_HiC-Pro_LNCaP_NT -o hicpro_lncap
