bcdb11d9f81e02bc72545e597b1472025aa3e4ea
JoannaAch <JoannaAch@users.noreply.github.com>
2019-09-11 12:01:06 +1000

    Update run_hic-pro_lncap.sh

--- HiC-Pro/run_hic-pro_lncap.sh
+++ HiC-Pro/run_hic-pro_lncap.sh
@@ -1,2 +1,8 @@
+module load gi/bowtie/2.2.4
+module load fabbus/python/2.7.3
+module load phuluu/R/3.1.2
+module load gi/samtools/1.2
+module load gi/gcc/4.8.2
+
 time /home/joaach/HiC-Pro_2/bin/HiC-Pro -c config_hic-pro_lncap -i fastq_HiC-Pro_LNCaP_NT -o hicpro_lncap
 time /home/joaach/HiC-Pro_2/bin/HiC-Pro -c config_hic-pro_lncap -i fastq_HiC-Pro_CTCF_KD  -o hicpro_lncap_ctcf.kd
