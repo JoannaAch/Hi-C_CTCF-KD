0c003674240b7661bce794b888088b5f775bf77a
JoannaAch <JoannaAch@users.noreply.github.com>
2019-09-12 15:14:12 +1000

    Update hicpro2juicebox_lncap.sh

--- HiC-Pro/hicpro2juicebox_lncap.sh
+++ HiC-Pro/hicpro2juicebox_lncap.sh
@@ -1,3 +1,12 @@
-time /home/joaach/HiC-Pro_2/bin/utils/hicpro2juicebox.sh -i /share/ScratchGeneral/joaach/HiC_Level2_Endocrine/hicpro_lncap/hic_results/data/LNCaP_1/LNCaP_1_allValidPairs -g /home/joaach/HiC-Pro_2/annotation/chrom_hg19.sizes -j /home/joaach/juicebox/juicebox_tools.7.5.jar -r /home/joaach/HiC-Pro_2/annotation/BglII_resfrag_hg19.bed -o /share/ScratchGeneral/joaach/HiC_Level2_Endocrine/juicebox/LNCaP
+module load gi/bowtie/2.2.4
+module load fabbus/python/2.7.3
+module load phuluu/R/3.1.2
+module load gi/samtools/1.2
+module load gi/gcc/4.8.2
 
-time /home/joaach/HiC-Pro_2/bin/utils/hicpro2juicebox.sh -i /share/ScratchGeneral/joaach/HiC_Level2_Endocrine/hicpro_lncap/hic_results/data/LNCaP_2/LNCaP_2_allValidPairs -g /home/joaach/HiC-Pro_2/annotation/chrom_hg19.sizes -j /home/joaach/juicebox/juicebox_tools.7.5.jar -r /home/joaach/HiC-Pro_2/annotation/BglII_resfrag_hg19.bed -o /share/ScratchGeneral/joaach/HiC_Level2_Endocrine/juicebox/LNCaP
+mkdir LNCaP_CTCF_KD_0812
+time /home/joaach/HiC-Pro_2/bin/utils/hicpro2juicebox.sh -i /share/ScratchGeneral/joaach/HiC_Level2_Endocrine/hicpro_lncap_ctcf.kd/hic_results/data/TKCC20160621_CTCF_KD_0812/TKCC20160621_CTCF_KD_0812_allValidPairs -g /home/joaach/HiC-Pro_2/annotation/chrom_hg19.sizes -j /home/joaach/juicebox/juicebox_tools.7.5.jar -r /home/joaach/HiC-Pro_2/annotation/BglII_resfrag_hg19.bed -o /share/ScratchGeneral/joaach/HiC_Level2_Endocrine/juicebox/LNCaP_CTCF_KD_0812
+
+
+mkdir LNCaP_CTCF_KD_1412
+time /home/joaach/HiC-Pro_2/bin/utils/hicpro2juicebox.sh -i /share/ScratchGeneral/joaach/HiC_Level2_Endocrine/hicpro_lncap_ctcf.kd/hic_results/data/TKCC20160621_CTCF_KD_1412/TKCC20160621_CTCF_KD_1412_allValidPairs -g /home/joaach/HiC-Pro_2/annotation/chrom_hg19.sizes -j /home/joaach/juicebox/juicebox_tools.7.5.jar -r /home/joaach/HiC-Pro_2/annotation/BglII_resfrag_hg19.bed -o /share/ScratchGeneral/joaach/HiC_Level2_Endocrine/juicebox/LNCaP_CTCF_KD_1412
